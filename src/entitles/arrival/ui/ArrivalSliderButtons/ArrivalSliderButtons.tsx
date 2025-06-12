8765192b3ac7b64f44950bb772c399a7809aec5b
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2025-06-13 01:17:15 +0300

    fix nav button for slider

--- src/entitles/arrival/ui/ArrivalSliderButtons/ArrivalSliderButtons.tsx
+++ src/entitles/arrival/ui/ArrivalSliderButtons/ArrivalSliderButtons.tsx
@@ -3,8 +3,8 @@ import './ArrivalSliderButtons.module.scss'
 export const ArrivalSliderButtons = () => {
 	return (
 		<>
-			<button className='arrow-prev'></button>
-			<button className='arrow-next'></button>
+			<button className='arrow-prev' aria-label='Previous slide'></button>
+			<button className='arrow-next' aria-label='Next slide'></button>
 		</>
 	)
 }
