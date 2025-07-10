a27521630a00ec3c62f99be526201c3f1b458117
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2025-07-11 01:06:36 +0300

    end maybe ? 6

--- src/app/AppEntry.tsx
+++ src/app/AppEntry.tsx
@@ -1,10 +1,5 @@
 import {AppProvider} from '@/app/providers'
 import ReactDOM from 'react-dom/client'
-import 'swiper/css'
-import 'swiper/css/effect-fade'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'swiper/css/thumbs'
 import './styles/base.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<AppProvider />)
