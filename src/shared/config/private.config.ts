66d41349b5ba743fe25afea398f3c799db8cc7ce
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2025-07-11 00:58:04 +0300

    end maybe ? 4

--- src/shared/config/private.config.ts
+++ src/shared/config/private.config.ts
@@ -3,10 +3,6 @@ export const APP_URL = process.env.APP_URL || 'http://localhost:5173'
 export const APP_DOMAIN = process.env.APP_DOMAIN || 'localhost'
 export const SERVER_URL = process.env.SERVER_URL || 'http://localhost:5001'
 
-export const ENABLE_PWA = process.env.ENABLE_PWA === 'true'
-export const ENABLE_SERVICE_WORKER =
-	process.env.ENABLE_SERVICE_WORKER === 'true'
-
 export const isDevelopment = ENV === 'development'
 export const isProduction = ENV === 'production'
 export const isStaging = ENV === 'staging'
